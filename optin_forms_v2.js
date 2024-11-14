e7ac728c7f59976d4bd4d20f75579e38576351f5
sashage <sashage@users.noreply.github.com>
2024-11-14 20:24:45 +0100

    update email extraction

--- optin_forms_v2.js
+++ optin_forms_v2.js
@@ -417,7 +417,7 @@
             // Use URLSearchParams to get the value of the 'he' parameter
             var email = urlObj.searchParams.get("he");
 
-            if ( email ) { email = decodeURIComponent(email); }
+            if ( email ) { email = decodeURIComponent(email).replace("%40","@"); }
 
             // If the email parameter exists in the URL
             return email || null;
