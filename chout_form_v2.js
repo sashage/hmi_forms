514fee07f726116f5a22323d6e2ab4566fb78881
sashage <sashage@users.noreply.github.com>
2025-07-09 14:46:13 +0200

    Pull impact click ID from URL in addition to Cookie

--- chout_form_v2.js
+++ chout_form_v2.js
@@ -364,7 +364,7 @@ function buildTrackingObject() {
         "cookie_fbp": generateFBPCookie(),
         "cookie_fbc": generateFBCCookie(),
 	"cookie_ttp": getCookieValue("_ttp"),
-	"cookie_impact_cid": getCookieValue("impact_cid"),
+	"cookie_impact_cid": getCookieValue("impact_cid") || getUrlParameter('irclickid'),
         "ga_client_id": setClientIdCookie(),
         "ga_session_id": getSessionID(gaMeasurementId),
         "utm_source": getUtmOrElValues("utm_source"),
